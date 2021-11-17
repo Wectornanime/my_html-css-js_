a3064e6a87c8a0e03edfeed86173b7bb9caf7315
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2021-11-17 20:20:05 -0300

    completo

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ hamburguer.addEventListener("click", function(){
     document.querySelector(".slidebar").classList.toggle("show-menu");
     document.querySelector(".hamburguer").classList.toggle("show-menu");
 });
+
+document.querySelector(".media-play").addEventListener("click", function(){
+    document.querySelector(".controls").classList.toggle("play")
+});
+
+document.querySelector(".media-pause").addEventListener("click", function(){
+    document.querySelector(".controls").classList.toggle("play")
+});
